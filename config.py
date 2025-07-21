6d2a9302ea49b22ff44fef2803fc7c1b7b2a33a1
Martelli Corp. <61651652+M0rtelli@users.noreply.github.com>
2025-07-21 19:05:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ if is_server is False:
     sql_data_path = "../data/solemon.db"
 else:
     sql_data_path = '../data/solemon.db'
-token = "5688874879:AAFC8QwvSX9F4Cbs9lN7dBETbTSu2VF0_-U" #main bot token
+token = "" #main bot token
 bot_nickname = "Solemon_Shop_Bot"
